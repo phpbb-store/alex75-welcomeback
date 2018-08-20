4892b7bfbd6c240791f9959c3e2d55460fb4b089
phpbb-store <alessandroinfostrada@libero.it>
2018-08-20 23:25:41 +0200

    Update listener.php

--- event/listener.php
+++ event/listener.php
@@ -9,8 +9,14 @@
 
 namespace alex75\welcomeback\event;
 
+/**
+ * @ignore
+ */
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * welcome back Event listener.
+ */
 class listener implements EventSubscriberInterface
 {
 
@@ -21,33 +27,29 @@ class listener implements EventSubscriberInterface
 	protected $config;
 	protected $language;
 
-
 	public function __construct(
 		\phpbb\user $user,
 		\phpbb\config\config $config,
 		\phpbb\template\template $template,
 		$root_path,
 		$phpEx,
-		$language)
+		\phpbb\language\language $language)
 	{
-		$this->user = $user;
-		$this->config = $config;
-		$this->template = $template;
-		$this->root_path = $root_path;
-		$this->phpEx = $phpEx;
-		$this->language	= $language;
+		$this->user			= $user;
+		$this->config		= $config;
+		$this->template		= $template;
+		$this->root_path	= $root_path;
+		$this->phpEx		= $phpEx;
+		$this->language		= $language;
 	}
-
 
 	static public function getSubscribedEvents()
 	{
 		return array(
 			'core.user_setup'	=> 'load_language',
-			'core.page_header'	=> 'build_urls', 
-			'core.page_header_after' => 'wb_sitename', 
+			'core.page_header'	=> 'wb_tpl_vars', 
 		);
 	}
-
 
 	public function load_language($event)
 	{
@@ -59,16 +61,12 @@ class listener implements EventSubscriberInterface
 		$event['lang_set_ext'] = $lang_set_ext;
 	}
 
-	public function build_urls($event)
+	public function wb_tpl_vars($event)
 	{
 		$url_1 = append_sid($this->root_path . 'search.' . $this->phpEx, 'author=' . $this->user->data['username'] . '&amp;sf=firstpost&amp;sr=topics');
-
 		$url_2 = append_sid($this->root_path . "ucp." . $this->phpEx, "i=ucp_main&amp;mode=drafts");
-
 		$url_3 = append_sid($this->root_path . "ucp." . $this->phpEx, "i=ucp_main&amp;mode=subscribed");
-		
 		$url_4 = append_sid($this->root_path . "ucp." . $this->phpEx, "i=ucp_main&amp;mode=bookmarks");
-		
 		$url_5 = append_sid($this->root_path . "ucp." . $this->phpEx, "i=ucp_attachments&amp;mode=attachments");
 
 		$this->template->assign_vars(array(
@@ -77,13 +75,7 @@ class listener implements EventSubscriberInterface
 			'U_WBYOURSUBSCRIBED'	=> $url_3,
 			'U_WBYOURBOOKMARKS'		=> $url_4,
 			'U_WBYOURATTACHMENTS'	=> $url_5,
+			'WB_SITENAME'			=> $this->language->lang('WELCOMEBACK_ON', $this->config['sitename']),
 		));
-	}
-	
-	public function wb_sitename($event)
-	{
-	$this->template->assign_vars(array(
-	'WB_SITENAME' => $this->language->lang('WELCOMEBACK_ON', $this->config['sitename']),
-	));
 	}
 }
